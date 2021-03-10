b858bec34a5f6e03eea4386678a9182eee2d6eed
Robert <ememm1236@gmail.com>
2021-03-10 12:34:49 +0300

    First hw

---
+++ QSLib/Library.fs
@@ -0,0 +1,28 @@
+﻿namespace QSLib
+
+open System
+module QuickSort =
+    let partition (arr:array<int>, small: int, big: int): int =
+        let pivot = arr.[big]
+        let mutable i = small - 1
+        let mutable med = 0
+        for j in small .. (big - 1) do
+            if arr.[j] < pivot then 
+                i <- i + 1
+                med <- arr.[i]
+                arr.[i] <- arr.[j]
+                arr.[j] <- med
+        med <- arr.[i+1]
+        arr.[i+1] <- arr.[big]
+        arr.[big] <- med
+        (i+1)
+
+
+    let rec QSort (arr: array<int>, small: int, big: int) : array<int> =
+        if small >= big then
+            arr
+        else
+            let PivotInd = partition(arr, small, big)
+            QSort(arr, small, (PivotInd - 1))
+            QSort(arr, (PivotInd + 1), big)
+            arr
---
+++ QScheck/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using QSLib;
+
+namespace QScheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            Random r = new Random();
+            for(int i=0; i < 10; i++)
+            {
+                arr[i] = r.Next(20);
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            arr = QSLib.QuickSort.QSort(arr, 0, 9);
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
