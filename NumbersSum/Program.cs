486791199d7e896e4dff270dc0b31a606276db25
Robert <ememm1236@gmail.com>
2021-04-13 14:44:55 +0300

    TwoNumbers and NumbersSum directorys having task with Redis.

---
+++ NumbersSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace NumbersSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+            IDatabase db = redis.GetDatabase();
+
+
+            Console.WriteLine("Введите 'exit' для выхода.");
+            while (Console.ReadLine() != "exit")
+            {
+                int num1 = Convert.ToInt32(db.StringGet("num1").ToString());
+                int num2 = Convert.ToInt32(db.StringGet("num2").ToString());
+
+                Console.WriteLine($"Сумма: {num1 + num2}");
+            }
+        }
+    }
+}
---
+++ TwoNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace TwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+            IDatabase db = redis.GetDatabase();
+
+
+            Console.WriteLine("Введите 'exit' для выхода.");
+            while (Console.ReadLine() != "exit")
+            {
+                Console.Write("Введите первое число: ");
+                db.StringSet("num1", Console.ReadLine());
+                Console.Write("Введите второе число: ");
+                db.StringSet("num2", Console.ReadLine());
+            }
+        }
+    }
+}
